63c10f936607f79a61838bb9abde532ad88a425f
Matt Wittmann <mcwitt@gmail.com>
2021-12-05 23:28:46 -0800

    O(log N) solution for 2021 day 6

--- src/AOC/Y2021/D06.hs
+++ src/AOC/Y2021/D06.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module AOC.Y2021.D06 where
@@ -7,10 +6,10 @@ import AOC.Solution (Day (Day), Solution (Solution, day, year), Year (Year))
 import qualified AOC.Solution as S
 import qualified Data.Attoparsec.ByteString.Char8 as P
 import Data.ByteString (ByteString)
-import Data.Foldable (fold)
-import Data.IntMap.Monoidal (MonoidalIntMap)
 import qualified Data.IntMap.Monoidal as IntMap
-import Data.Monoid (Sum (..))
+import qualified Data.List as List
+import Data.Maybe (fromMaybe)
+import Data.Semigroup (mtimesDefault)
 
 parseInput :: ByteString -> [Int]
 parseInput s = let Right r = P.parseOnly input s in r
@@ -21,19 +20,38 @@ solve1 :: [Int] -> Int
 solve1 = solve 80
 
 solve :: Int -> [Int] -> Int
-solve gen =
-  getSum
-    . fold
-    . (!! gen)
-    . iterate step
-    . foldMap (`IntMap.singleton` Sum 1)
+solve gen ts =
+  let initCounts = IntMap.fromListWith (+) $ zip ts $ repeat (1 :: Int)
+      initState = [fromMaybe 0 $ IntMap.lookup i initCounts | i <- [0 .. 8]]
+      propagator = mtimesDefault gen transition
+   in sum $ matrixVectorProduct propagator initState
 
-step :: MonoidalIntMap (Sum Int) -> MonoidalIntMap (Sum Int)
-step = foldMap go . IntMap.toList
-  where
-    go = \case
-      (0, n) -> IntMap.fromList [(8, n), (6, n)]
-      (t, n) -> IntMap.singleton (t - 1) n
+newtype IntMatrix = M [[Int]] deriving (Show)
+
+instance Semigroup IntMatrix where
+  M x <> M y = M [[sum $ zipWith (*) r c | c <- List.transpose y] | r <- x]
+
+instance Monoid IntMatrix where
+  mempty = undefined
+
+type IntVector = [Int]
+
+matrixVectorProduct :: IntMatrix -> IntVector -> IntVector
+matrixVectorProduct (M rs) v = fmap (sum . zipWith (*) v) rs
+
+transition :: IntMatrix
+transition =
+  M
+    [ [0, 1, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 1, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
 
 solve2 :: [Int] -> Int
 solve2 = solve 256
