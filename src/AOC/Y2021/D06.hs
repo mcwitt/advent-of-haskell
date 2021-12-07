0e3dc2306eec52a3f8479f15f0111d50b6126588
Matt Wittmann <mcwitt@gmail.com>
2021-12-06 16:47:04 -0800

    Simplify 2021 day 6

--- src/AOC/Y2021/D06.hs
+++ src/AOC/Y2021/D06.hs
@@ -6,9 +6,7 @@ import AOC.Solution (Day (Day), Solution (Solution, day, year), Year (Year))
 import qualified AOC.Solution as S
 import qualified Data.Attoparsec.ByteString.Char8 as P
 import Data.ByteString (ByteString)
-import qualified Data.IntMap.Monoidal as IntMap
 import qualified Data.List as List
-import Data.Maybe (fromMaybe)
 import Data.Semigroup (mtimesDefault)
 
 parseInput :: ByteString -> [Int]
@@ -16,17 +14,17 @@ parseInput s = let Right r = P.parseOnly input s in r
   where
     input = P.decimal `P.sepBy` ","
 
-solve1 :: [Int] -> Int
+solve1 :: [Int] -> Integer
 solve1 = solve 80
 
-solve :: Int -> [Int] -> Int
+solve :: Int -> [Int] -> Integer
 solve gen ts =
-  let initCounts = IntMap.fromListWith (+) $ zip ts $ repeat (1 :: Int)
-      initState = [fromMaybe 0 $ IntMap.lookup i initCounts | i <- [0 .. 8]]
+  let count x = length . filter (== x)
+      initState = [fromIntegral $ count t ts | t <- [0 .. 8]]
       propagator = mtimesDefault gen transition
    in sum $ matrixVectorProduct propagator initState
 
-newtype IntMatrix = M [[Int]] deriving (Show)
+newtype IntMatrix = M [[Integer]] deriving (Show)
 
 instance Semigroup IntMatrix where
   M x <> M y = M [[sum $ zipWith (*) r c | c <- List.transpose y] | r <- x]
@@ -34,7 +32,7 @@ instance Semigroup IntMatrix where
 instance Monoid IntMatrix where
   mempty = undefined
 
-type IntVector = [Int]
+type IntVector = [Integer]
 
 matrixVectorProduct :: IntMatrix -> IntVector -> IntVector
 matrixVectorProduct (M rs) v = fmap (sum . zipWith (*) v) rs
@@ -53,10 +51,10 @@ transition =
       [1, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
 
-solve2 :: [Int] -> Int
+solve2 :: [Int] -> Integer
 solve2 = solve 256
 
-solution :: Solution [Int] Int Int
+solution :: Solution [Int] Integer Integer
 solution =
   Solution
     { year = Year 2021,
