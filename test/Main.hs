b760e959042f427c5069f1fe159e95267df449f5
Matt Wittmann <mcwitt@gmail.com>
2022-02-11 20:12:21 -0800

    Add 2021 day 11

--- test/Main.hs
+++ test/Main.hs
@@ -7,6 +7,7 @@ import AOC.Y2015.D04.Tests
 import AOC.Y2015.D06.Tests
 import AOC.Y2021.D03.Tests
 import AOC.Y2021.D09.Tests
+import AOC.Y2021.D11.Tests
 import Test.Tasty
 
 main :: IO ()
@@ -20,5 +21,6 @@ main =
         AOC.Y2015.D04.Tests.tests,
         AOC.Y2015.D06.Tests.tests,
         AOC.Y2021.D03.Tests.tests,
-        AOC.Y2021.D09.Tests.tests
+        AOC.Y2021.D09.Tests.tests,
+        AOC.Y2021.D11.Tests.tests
       ]
